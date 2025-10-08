073bcc7dac42e3a95efba8bb15c112ddec60dd7d
aaliyahazzahra <aariariaa02@gmail.com>
2025-10-08 10:35:35 +0700

    TUGAS3

--- coba.dart
+++ coba.dart
@@ -1,48 +1,9 @@
-
+// Soal 2 – Cetak Karakter: Solusi paling efisien menggunakan String repeat.
 void main() {
-  sapa("Aaliyah");
-  biodata();
+  // Membuat string "* " dan mengulanginya 5 kali, menghasilkan "* * * * * ".
+  String hasil = "* ".repeat(5);
   
+  // print() mencetak string, dan trimRight() menghilangkan spasi ekstra di paling kanan.
+  print(hasil.trimRight());
 }
 
-void sapa(nama) {
-  print("Halo, $nama");
-  print("BIODATA");
-}
-
-void biodata() {
-  // Menyimpan nama pengguna
-  //String menandakan sebuah teks
-  String nama = "Aaliyah R. ' A. Azzahra";
-
-  //int menandakan sebuah angka (bilangan bulat)
-  int usia = 25;
-
-  //double menandakan sebuah angka (bilangan desimal)
-  double TinggiBadan = 1.52;
-
-  //bool hanya memiliki dua nilai 'true' dan 'false
-  bool Aktif = true;
-  bool TidakAktif = false;
-
-  //List adalah kumpulan data
-  List<String> bukufav = ["Biru Laut", "Matahari", "Bulan"];
- 
-  //Map, menyimpan key-value
-  Map<String, dynamic> biodata = {
-    'profesi': ["Mahasiswa", "Designer", "Editor"],
-    'alamat': {
-      'asalkota': 'Medan',
-      'domisili': 'Jakarta',
-      },
-  };
-
-  // Mencetak biodata ke konsol
-  print("Nama: $nama");
-  print("Usia: $usia tahun");
-  print("Tinggi: $TinggiBadan m");
-  print("Mahasiswa: $Aktif");
-  print("Buku Favorit : ${bukufav[0]}");
-  print("Profesi : ${biodata['profesi'][0]}");
-  print("Alamat : ${biodata['alamat']['domisili']}");
-}
